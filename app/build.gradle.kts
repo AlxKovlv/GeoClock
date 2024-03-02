f15418b52f2db2ddfb5419bc5e0a4e852ecf0f7d
AlxKovlv <alexeyko@my.hit.ac.il>
2024-03-02 14:16:56 +0200

    gradle sync with Firebase SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,21 +43,15 @@ android {
 
 dependencies {
 
+    //Default
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-firestore:24.10.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-
-    //room library
-    implementation ("androidx.room:room-runtime:2.6.1")
-    kapt ("androidx.room:room-compiler:2.6.1")
-
-    // Kotlin Extensions and Coroutines support for Room
-    implementation ("androidx.room:room-ktx:2.6.1")
 
     //ViewModel and livedata
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
