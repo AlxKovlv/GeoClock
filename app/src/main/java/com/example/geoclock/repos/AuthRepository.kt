172c8d47ea89605d26b4b4ae7c2e8eb6a8a8cce7
AlxKovlv <alexeyko@my.hit.ac.il>
2024-03-05 18:43:50 +0200

    auth repository impl done

---
+++ app/src/main/java/com/example/geoclock/repos/AuthRepository.kt
@@ -0,0 +1,12 @@
+package com.example.geoclock.repos
+
+import com.example.geoclock.model.User
+import com.example.geoclock.util.Resource
+
+interface AuthRepository {
+
+    suspend fun currentUser() : Resource<User>
+    suspend fun login(email:String, password:String) : Resource<User>
+    suspend fun createUser(userName:String, userEmail:String, userPhone:String, userLoginPass:String)
+    fun logout()
+}
---
+++ app/src/main/java/com/example/geoclock/repos/FirebaseImpl/AuthRepositoryFirebase.kt
@@ -0,0 +1,59 @@
+package com.example.geoclock.repos.FirebaseImpl
+
+import com.example.geoclock.model.User
+import com.example.geoclock.repos.AuthRepository
+import com.example.geoclock.util.Resource
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import safeCall
+
+class AuthRepositoryFirebase: AuthRepository {
+
+    private val firebaseAuth = FirebaseAuth.getInstance()
+    private val userRef = FirebaseFirestore.getInstance().collection("users")
+
+
+
+    override suspend fun currentUser(): Resource<User> {
+        return withContext(Dispatchers.IO){
+            safeCall {
+                val user = userRef.document(firebaseAuth.currentUser!!.uid).get().await().toObject(User::class.java)
+                Resource.Success(user!!)
+            }
+        }
+    }
+
+    override suspend fun login(email: String, password: String): Resource<User> {
+        return withContext(Dispatchers.IO){
+            safeCall {
+                val result = firebaseAuth.signInWithEmailAndPassword(email, password).await()
+                val user = userRef.document(result.user?.uid!!).get().await().toObject(User::class.java)!!
+                Resource.Success(user)
+            }
+        }
+    }
+
+    override suspend fun createUser(
+        userName: String,
+        userEmail: String,
+        userPhone: String,
+        userLoginPass: String
+    ) {
+        return withContext(Dispatchers.IO){
+            safeCall {
+                val registrationResult = firebaseAuth.createUserWithEmailAndPassword(userEmail, userLoginPass).await()
+                val userId = registrationResult.user?.uid!!
+                val newUser = User(userName, userEmail, userPhone)
+                userRef.document(userId).set(newUser).await()
+                Resource.Success(newUser)
+            }
+        }
+    }
+
+    override fun logout() {
+        firebaseAuth.signOut()
+    }
+}
