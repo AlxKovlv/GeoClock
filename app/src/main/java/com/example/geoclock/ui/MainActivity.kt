98e075754522e03b4b2e34dce18a087fb826af14
AlxKovlv <alexeyko@my.hit.ac.il>
2024-03-29 20:36:11 +0300

    UI updates (in progress)

--- app/src/main/java/com/example/geoclock/ui/MainActivity.kt
+++ app/src/main/java/com/example/geoclock/ui/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.example.geoclock.ui
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsetsController
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.geoclock.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Set custom color for the navigation bar
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val window = window
+            val controller = window.insetsController
+            if (controller != null) {
+                controller.setSystemBarsAppearance(
+                    WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS,
+                    WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS
+                )
+                window.navigationBarColor = ContextCompat.getColor(this, R.color.my_primary_color)
+            }
+        }
 
     }
 }
